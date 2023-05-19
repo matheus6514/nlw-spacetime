87abf30fb17c54b5bce8d830beaa6d6231cacdff
Matheus Goncalves <matheus_gon97@hotmail.com>
2023-05-18 23:10:50 -0300

    criando middleware e novas funcionalidades para o frontend

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 
-import nlwLogo from '../assets/nlw-spacetime-log.svg'
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import Link from 'next/link'
 
 export function Hero() {
   return (
     <div className="space-y-5">
-      <Image src={nlwLogo} alt="NLW Spacetime" />
+      <Image src={nlwLogo} alt="teste" />
 
       <div className="max-w-[420px] space-y-1">
         <h1 className="text-5xl font-bold leading-tight text-gray-50">
@@ -17,12 +18,12 @@ export function Hero() {
         </p>
       </div>
 
-      <a
+      <Link
         className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
-        href=""
+        href="/memories/new"
       >
         CADASTRAR LEMBRANÇA
-      </a>
+      </Link>
     </div>
   )
 }
