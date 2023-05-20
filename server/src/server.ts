1368fcae6d2923196d3c9470f2b791e36d53f03b
Matheus Goncalves <matheus_gon97@hotmail.com>
2023-05-19 23:29:02 -0300

    Rota de upload de arquivos e sevindo arquivos estáticos

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,19 @@ import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true, // todas as URLs de fron-end poderão acessar nosso back-end
@@ -16,8 +26,9 @@ app.register(jwt, {
   secret: 'spacetime',
 })
 
-app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
 
 app
   .listen({
